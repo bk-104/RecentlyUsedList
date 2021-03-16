babe67ba2ae91348d4c25c83011d67efcdf6bf04
Byron Glover <byron.glover@newsignature.com>
2021-03-16 11:26:54 +0000

    beEmptyWhenInitialised Test created

--- src/main/java/com/oocode/RecentlyUsedList.java
+++ src/main/java/com/oocode/RecentlyUsedList.java
@@ -3,7 +3,7 @@ package com.oocode;
 import java.util.Collection;
 
 public class RecentlyUsedList {
-	public Collection<Object> getEntries() {
+	public Integer length() {
 		return null;
 	}
 }
---
+++ src/test/java/com/oocode/ListShould.java
@@ -0,0 +1,40 @@
+package com.oocode;
+
+import org.hamcrest.collection.IsEmptyCollection;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ListShould {
+	@Test
+	public void canAnswerTheUniversalQuestion() {
+		assertThat(new Example().answer(), is(42));
+	}
+
+	@Test
+	// The list should be empty when initialised
+	public void beEmptyWhenInitialised() {
+		RecentlyUsedList list = new RecentlyUsedList();
+		assertThat(list.length(), is(0));
+	}
+
+	//We should be able to add things to the list
+	public void ableToAddThingsToTheList() {
+		RecentlyUsedList list = new RecentlyUsedList();
+	}
+	//We should be able to retrieve items from the list
+	public void ableToRetrieveItemsFromTheList() {
+
+	}
+
+	//The most recent item should be first in the list
+	public void mostRecentItemAddedShouldReturnFirst() {
+
+	}
+
+	//Items in the list are unique, so duplicate insertions should be moved rather than added.
+	public void itemsInListAreUnique() {
+
+	}
+}
